1e0dbc1c8a53ab8de1e0368e657f313fb64744d8
naresh sharma <sobarguy.naresh@yahoo.co.in>
2019-11-17 10:28:53 +0530

    fifth commit

--- test1.js
+++ test1.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+function cal(num1,num2, caltype){
+
+if(caltype=="add"){
+
+
+    return num1+num2;
+}
+
+else if(caltype=="multi"){
+
+    return num1*num2;
+}
+
+
+};
+
+console.log(cal(2,3,"add"));
+
